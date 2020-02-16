9b2bc1b796ddbfcddfc7f4a6bb2c0cb6f9f06ea4
Richard Yhip <richard.yhip@gmail.com>
2020-02-16 13:59:22 -0500

    refactored SearchBooks into it's own component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Route, Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import BookList from './BookList'
+import SearchBooks from './SearchBooks'
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -9,28 +10,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route path='/' component={BookList} />
-        <Route path='/search' render={() => (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author" />
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        )} />
+        <Route path='/search' component={SearchBooks} />
       </div>
     )
   }
