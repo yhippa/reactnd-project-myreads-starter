c4354e0719083e9c3795893de38ccddcb7af36bd
Richard Yhip <richard.yhip@gmail.com>
2020-02-16 14:03:44 -0500

    basic link routing and reformatting

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class SearchBooks extends Component {
     render() {
-        return(
+        return (
             <div className="search-books">
-            <div className="search-books-bar">
-              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
-              <div className="search-books-input-wrapper">
-                {/*
+                <div className="search-books-bar">
+                    <Link to="/">
+                        <button className="close-search">Close</button>
+                    </Link>
+                    <div className="search-books-input-wrapper">
+                        {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -15,14 +18,14 @@ class SearchBooks extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author" />
+                        <input type="text" placeholder="Search by title or author" />
 
-              </div>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid"></ol>
+                </div>
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
         )
     }
 }
