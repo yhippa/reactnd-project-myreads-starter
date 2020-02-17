8170bae52af25e94e1729ba41159b1651ff1269c
Richard Yhip <richard.yhip@gmail.com>
2020-02-17 14:40:34 -0500

    fixed book display from api data

--- src/Book.js
+++ src/Book.js
@@ -4,16 +4,16 @@ import ChangeShelf from './ChangeShelf'
 class Book extends Component {
     render() {
         const { book, shelves, changeShelf } = this.props;
-
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={book.cover}></div>
+                    {console.log('book', book)}
+                        <div className="book-cover" style={{ width: '100%', height: '100%', backgroundImage:`url(${book.imageLinks.smallThumbnail})` }}></div>
                         <ChangeShelf book={book} changeShelf={changeShelf} shelves={shelves}/>                        
                     </div>
                     <div className="book-title">{book.title}</div>
-                    <div className="book-authors">{book.authors}</div>
+                    <div className="book-authors">{book.authors.join(", ")}</div>
                 </div>
             </li>
         )
